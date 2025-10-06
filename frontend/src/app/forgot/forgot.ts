ab9d155edf5e4b467f15e36e0de07935be9b7641
shyamilla <shyamilla08@gmail.com>
2025-10-06 14:47:34 +0000

    complete auth

--- frontend/src/app/forgot/forgot.ts
+++ frontend/src/app/forgot/forgot.ts
@@ -15,30 +15,20 @@ export class ForgotComponent {
   email = '';
   message = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
-
-  // sendOtp() {
-  //   this.authService.forgotPassword(this.email).subscribe({
-  //     next: () => {
-  //       this.message = 'OTP sent successfully!';
-  //       this.router.navigate(['/reset'], { state: { email: this.email } });
-  //     },
-  //     error: (err: any) => this.message = err.error || 'Failed to send OTP'
-  //   });
-  // }
+  constructor(private authService: AuthService, private router: Router) { }
 
 
   sendOtp() {
-  this.authService.forgotPassword({ email: this.email }).subscribe({
-    next: () => {
-      this.message = '✅ OTP sent successfully!';
-      this.router.navigate(['/reset'], { state: { email: this.email } });
-    },
-    error: (err: any) => {
-      console.error('Forgot password error:', err);
-      this.message = err.error?.message || '❌ Failed to send OTP';
-    }
-  });
-}
+    this.authService.forgotPassword({ email: this.email }).subscribe({
+      next: () => {
+        this.message = '✅ OTP sent successfully!';
+        this.router.navigate(['/reset'], { state: { email: this.email } });
+      },
+      error: (err: any) => {
+        console.error('Forgot password error:', err);
+        this.message = err.error?.message || '❌ Failed to send OTP';
+      }
+    });
+  }
 
 }
